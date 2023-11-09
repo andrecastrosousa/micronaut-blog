af17bed8e166dd1a6bb1c7bcb744c7e8a14141af
André Sousa <andre.castro.sousa@gmail.com>
2023-11-09 16:31:48 -0100

    add: author service logic

--- src/main/kotlin/com/andrecastrosousa/service/AuthorService.kt
+++ src/main/kotlin/com/andrecastrosousa/service/AuthorService.kt
@@ -1,7 +1,14 @@
 package com.andrecastrosousa.service
 
 import com.andrecastrosousa.model.Author
+import io.micronaut.core.annotation.NonNull
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 interface AuthorService {
     fun listAll() : List<Author>
+    fun getById(@NotBlank id: Long) : Author
+    fun create(@NotBlank author: Author) : Author
+    fun update(@NotNull @NonNull id: Long, @NotBlank author: Author) : Author
+    fun deleteById(@NotNull @NonNull id: Long)
 }
--- src/main/kotlin/com/andrecastrosousa/service/AuthorServiceImpl.kt
+++ src/main/kotlin/com/andrecastrosousa/service/AuthorServiceImpl.kt
@@ -7,4 +7,20 @@ class AuthorServiceImpl (private val authorRepository: AuthorRepository) : Autho
     override fun listAll() : List<Author> {
         return authorRepository.findAll().toList();
     }
+
+    override fun getById(id: Long): Author {
+        return authorRepository.findById(id).get()
+    }
+
+    override fun create(author: Author): Author {
+        return authorRepository.save(author)
+    }
+
+    override fun update(id: Long, author: Author): Author {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteById(id: Long) {
+        authorRepository.deleteById(id);
+    }
 }
