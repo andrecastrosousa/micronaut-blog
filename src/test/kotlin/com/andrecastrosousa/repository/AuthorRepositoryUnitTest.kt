92755f34bdb1454951da21d5f7fd335e284fe13d
André Sousa <andre.castro.sousa@gmail.com>
2023-11-10 12:11:29 -0100

    add: author repository unit test

---
+++ src/test/kotlin/com/andrecastrosousa/repository/AuthorRepositoryUnitTest.kt
@@ -0,0 +1,29 @@
+package com.andrecastrosousa.repository
+
+import io.micronaut.context.BeanContext
+import io.micronaut.data.annotation.Query
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class AuthorRepositoryUnitTest {
+
+    @Inject
+    lateinit var beanContext: BeanContext
+
+    @Test
+    fun testAnnotationMetadata() {
+        val query = beanContext.getBeanDefinition(AuthorRepository::class.java)
+            .getRequiredMethod<Any>("findByFirstname", String::class.java)
+            .annotationMetadata
+            .stringValue(Query::class.java)
+            .orElse(null)
+
+        assertEquals(
+            "SELECT author_ FROM com.andrecastrosousa.model.Author AS author_ WHERE (author_.firstname = :p1)",
+            query
+        )
+    }
+}
