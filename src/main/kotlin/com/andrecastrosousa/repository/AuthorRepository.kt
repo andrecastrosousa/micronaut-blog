9453160a7ee9110b4810ee13a040635b414ba27a
André Sousa <andre.castro.sousa@gmail.com>
2023-11-10 12:13:36 -0100

    remove: @Executable on queries for author repo

--- src/main/kotlin/com/andrecastrosousa/repository/AuthorRepository.kt
+++ src/main/kotlin/com/andrecastrosousa/repository/AuthorRepository.kt
@@ -1,28 +1,18 @@
 package com.andrecastrosousa.repository
 
 import com.andrecastrosousa.model.Author
-import io.micronaut.context.annotation.Executable
-import io.micronaut.core.annotation.NonNull
-import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.exceptions.DataAccessException
-import io.micronaut.data.jdbc.annotation.JdbcRepository
-import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.CrudRepository
-import io.micronaut.data.repository.PageableRepository
 import jakarta.transaction.Transactional
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 
 @Repository
 interface AuthorRepository : CrudRepository<Author, Long> {
-    @Executable
     fun save(@NotBlank author: Author) : Author
 
-    @Executable
     fun findByFirstname(firstname: String): Author
 
-    @Executable
     @Transactional
     fun saveWithException(@NotBlank author: Author) : Author {
         save(author)
